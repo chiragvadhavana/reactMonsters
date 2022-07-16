bb501f033fe9da1736e1c8b886ec95c4e5cd1578
chiragvadhavana <81073437+chiragvadhavana@users.noreply.github.com>
2022-07-16 12:32:30 +0530

    mapping

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import CardList from "./components/cardList/cardList.component";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const [monsters, setMonsters] = useState([]);
+
+  useEffect(() => {
+     fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => {
+        setMonsters(data);
+      });
+  }, []);
+
+  return <CardList monsters = {monsters}></CardList>
+};
 
 export default App;
---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,16 @@
+import "./card.styles.css";
+
+const Card = ({ id, name, email }) => {
+  return (
+    <div key={id} className="card-container">
+      <img
+        alt="monster"
+        src={`https://robohash.org/${id}?set=set2&size=180x180`}
+      />
+      <h2 className="name">{name}</h2>
+      <p className="email">{email}</p>
+    </div>
+  );
+};
+
+export default Card;
